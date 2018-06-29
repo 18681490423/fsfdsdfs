b45aa100152d2e8df3d35072275d3fe316753db9
RSMC9ULLSWY2H43\Administrator <1060591385@qq.com>
2018-06-29 17:37:09 +0800

    sss

---
+++ scripts/test_assert.py
@@ -0,0 +1,12 @@
+class TestCase:
+    def test_assert1(self):
+        assert 1
+
+    def test_assert2(self):
+        assert 1
+
+    def test_assert3(self):
+        assert 1
+
+    def test_assert4(self):
+        assert 0
